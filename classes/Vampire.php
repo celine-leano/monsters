dd150e3020db9d8a18b71b2ae3b2322d081c77ef
Alec <avan-etten@mail.greenriver.edu>
2019-02-06 14:19:55 -0500

    added vampire class

---
+++ classes/Vampire.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: A-VE
+ * Date: 2/6/2019
+ * Time: 11:15 AM
+ */
+
+class Vampire
+{
+    private $_victims;
+
+    function __construct($name)
+    {
+        parent::__construct($name);
+        $this->_victims = 0;
+    }
+
+    function attack()
+    {
+        echo "Your vampire attacks a victim";
+        $this->_victims++;
+    }
+
+    function getVictims()
+    {
+        return $this->_victims;
+    }
+}
